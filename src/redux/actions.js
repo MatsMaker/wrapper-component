4c3d5cd9682f66780ede2b6f875daa1faa603dfe
Sergey Matsitsky <matsitskys@mydigicode.com>
2019-06-05 12:58:04 +0300

    forgot to get the streams from server

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
 import { GET_STREAMS, CHANGE_GROUP } from './actionsTypes';
+import StreamData from './reducers/StreamData';
 
-export const getStreams = () => ({
-  type: GET_STREAMS
-});
+export const getStreams = () => {
+  return dispatch => {
+    axios.get(`https://predictoria.com/api/stream/`)
+      .then(res => {
+        dispatch({
+          type: GET_STREAMS,
+          payload: res,
+        });
+      })
+      .catch(error => {
+        dispatch({ // Of course it is only for some show =) because server not support CURS
+          type: GET_STREAMS,
+          payload: StreamData,
+        });
+      });
+  }
+}
 
 export const changeSelect = (e) => ({
   type: CHANGE_GROUP,
--- src/redux/reducers/streams.js
+++ src/redux/reducers/streams.js
@@ -13,7 +13,7 @@ export default function (state = initialState, action) {
     case GET_STREAMS: {
       return {
         ...state,
-        streamsList: StreamData,
+        streamsList: action.payload,
       }
     }
     case CHANGE_GROUP: {
